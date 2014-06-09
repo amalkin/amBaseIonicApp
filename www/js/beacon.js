b343a08cecad82a88ba6811fc1e042004a205833
Alastair Malkin <malkin@adobe.com>
2014-06-09 12:00:33 +0100

    Beacon functionality

--- www/js/beacon.js
+++ www/js/beacon.js
@@ -6,13 +6,17 @@ AttendeaseBeacons.notifyServerAuthToken(theAuthToken);
 
 uuidsToMonitor = ["E2C56DB5-DFFB-48D2-B060-D0F5A71096E0", "B9407F30-F5F8-466E-AFF9-25556B57FE6D"];
 
+var element = document.getElementById('beaconProperties');
+
 AttendeaseBeacons.monitor(uuidsToMonitor, function() {
 	return setInterval((function() {
 		return AttendeaseBeacons.getBeacons(function(beacons) {
 			if (_.isEmpty(beacons)) {
+				element.innerHTML = "No beacons found.";
 				return console.log("No beacons found.");
 			} else {
 				return _.each(beacons, function(beacon) {
+					element.innerHTML = "Beacons: " + beacon.uuid + " (" + beacon.major + ", " + beacon.minor + ") " + beacon.proximityString + " (" + beacon.accuracy + " meters)";
 					return console.log("" + beacon.uuid + " (" + beacon.major + ", " + beacon.minor + ") " + beacon.proximityString + " (" + beacon.accuracy + " meters)");
 				});
 			}
